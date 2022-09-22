fde0cb1ce106723f8fd695851968c9c61e3f515a
NucciTheBoss <jason.nucciarone@canonical.com>
2022-09-22 08:02:11 -0400

    Address suggested revisions in issue tracker
    Changes:
    * Drop auth actions.
    * Subsume munge under _service_start() and _service_stop().
    * Expand variable names for managers.

--- src/charm.py
+++ src/charm.py
@@ -8,7 +8,7 @@ import logging
 
 from hpctlib.misc import service_forced_update
 from hpctlib.ops.charm.service import ServiceCharm
-from ops.charm import ActionEvent, InstallEvent, StartEvent, StopEvent
+from ops.charm import InstallEvent, StartEvent, StopEvent
 from ops.framework import StoredState
 from ops.main import main
 
@@ -24,36 +24,8 @@ class SlurmServerCharm(ServiceCharm):
 
     def __init__(self, *args):
         super().__init__(*args)
-
-        self.framework.observe(self.on.auth_start_action, self._auth_start)
-        self.framework.observe(self.on.auth_stop_action, self._auth_stop)
-
-        self.manager = SlurmServerManager()
-        self.auth_manager = MungeManager()
-
-    @service_forced_update()
-    def _auth_start(self, event: ActionEvent) -> None:
-        """Fired when auth-start is run."""
-        self.service_set_state("idle")
-        self.service_set_status_message("Starting munge")
-        self.service_update_status()
-        self.auth_manager.start()
-
-        self.service_set_state("started")
-        self.service_set_status_message()
-        self.service_update_status()
-
-    @service_forced_update()
-    def _auth_stop(self, event: ActionEvent) -> None:
-        """Fired when auth-stop is run."""
-        self.service_set_state("idle")
-        self.service_set_status_message("Stopping munge")
-        self.service_update_status()
-        self.auth_manager.stop()
-
-        self.service_set_state("broken")
-        self.service_set_status_message("Authentication service is not active.")
-        self.service_update_status()
+        self.slurm_server_manager = SlurmServerManager()
+        self.munge_manager = MungeManager()
 
     @service_forced_update()
     def _service_install(self, event: InstallEvent) -> None:
@@ -61,11 +33,11 @@ class SlurmServerCharm(ServiceCharm):
         self.service_set_state("waiting")
         self.service_set_status_message("Installing munge")
         self.service_update_status()
-        self.auth_manager.install()
+        self.munge_manager.install()
 
         self.service_set_status_message("Installing slurmctld")
         self.service_update_status()
-        self.manager.install()
+        self.slurm_server_manager.install()
 
         self.service_set_state("started")
         self.service_set_status_message()
@@ -75,9 +47,13 @@ class SlurmServerCharm(ServiceCharm):
     def _service_start(self, event: StartEvent) -> None:
         """Fired when service-start is run."""
         self.service_set_state("idle")
+        self.service_set_status_message("Starting munge")
+        self.service_update_status()
+        self.munge_manager.start()
+
         self.service_set_status_message("Starting slurmctld")
         self.service_update_status()
-        self.manager.start()
+        self.slurm_server_manager.start()
 
         self.service_set_state("started")
         self.service_set_status_message()
@@ -89,7 +65,11 @@ class SlurmServerCharm(ServiceCharm):
         self.service_set_state("idle")
         self.service_set_status_message("Stopping slurmctld")
         self.service_update_status()
-        self.manager.stop()
+        self.slurm_server_manager.stop()
+
+        self.service_set_status_message("Stopping munge")
+        self.service_update_status()
+        self.munge_manager.stop()
 
         self.service_set_state("broken")
         self.service_set_status_message("Slurm server is not active.")
