ad293bd293ec818174dcf6b07f793ee99d5f53a5
NucciTheBoss <jason.nucciarone@canonical.com>
2022-10-05 16:31:24 -0400

    Enhance munge manager
    Changes:
    * Drop properties.
    * Change hash to get_hash().
    * Reduce errors to just a base class.
    * Improve docstrings.
    * Use one-liners where appropriate instead of code blocks.

--- src/manager/munge.py
+++ src/manager/munge.py
@@ -7,8 +7,8 @@
 import hashlib
 import logging
 import os
-import stat
 import subprocess
+from typing import Union
 
 import charms.operator_libs_linux.v0.apt as apt
 from charms.operator_libs_linux.v1.systemd import (
@@ -22,158 +22,128 @@ from hpctinterfaces.ext.file import FileDataInterface
 logger = logging.getLogger(__name__)
 
 
-class MungeNotFoundError(Exception):
-    """Raised when trying to perform an operation with munge but munge is not installed."""
+class MungeManagerError(Exception):
+    """Raised when the munge manager encounters an error."""
 
-    def __init__(self, name: str, desc: str = "Munge is not installed.") -> None:
-        self.name = name
-        self.desc = desc
-        super().__init__(self.desc)
-
-    def __str__(self) -> str:
-        """String representation of MungeNotFoundError."""
-        return f"{self.desc} Please install munge using {self.name}.install()."
-
-
-class MungeKeyNotFoundError(Exception):
-    """Raised when manager cannot locate the `munge.key` file on a unit."""
-
-    def __init__(self, path: str, desc: str = "Munge key not found on host.") -> None:
-        self.path = path
-        self.desc = desc
-        super().__init__(self.desc)
-
-    def __str__(self) -> str:
-        """String representation of MungeKeyNotFoundError."""
-        return f"{self.desc} Location searched: {self.path}"
+    ...
 
 
 class MungeManager:
     """Top-level manager class for controlling munge on unit."""
 
-    @property
-    def installed(self) -> bool:
-        """Installation status of munge.
+    def __init__(self) -> None:
+        self.key = "/etc/munge/munge.key"
+
+    def get_hash(self, file: Union[str, None] = None) -> Union[str, None]:
+        """Get the sha224 hash of a file.
+
+        Args:
+            str | None: File to hash. Defaults to self.key if file is None.
 
         Returns:
-            bool: True if munge is installed on unit; False if munge is not installed on unit.
+            str | None: sha224 hash of the file, or None if file does not exist.
         """
-        return self.__is_installed()
-
-    @property
-    def key(self) -> str:
-        """Location of munge key file on unit.
-
-        Raises:
-            MungeKeyNotFoundError: Thrown if `/etc/munge/munge.key` does not exist on unit.
-
-        Returns:
-            str: Path to munge key file on unit.
-        """
-        if os.path.isfile("/etc/munge/munge.key"):
-            return "/etc/munge/munge.key"
-        else:
-            raise MungeKeyNotFoundError("/etc/munge/munge.key")
-
-    @property
-    def hash(self) -> str:
-        """sha224 hash of `munge.key`.
-
-        Raises:
-            MungeKeyNotFoundError: Thrown if `/etc/munge/munge.key` does not exist on unit.
-
-        Returns:
-            str: sha224 hash of `munge.key`.
-        """
-        if os.path.isfile("/etc/munge/munge.key"):
-            sha224 = hashlib.sha224()
-            sha224.update(open("/etc/munge/munge.key", "rb").read())
-            return sha224.hexdigest()
-        else:
-            raise MungeKeyNotFoundError("/etc/munge/munge.key")
+        file = self.key if file is None else file
+        return (
+            hashlib.sha224(open(file, "rb").read()).hexdigest() if os.path.isfile(file) else None
+        )
 
     def generate_new_key(self) -> None:
         """Generate a new munge.key file using `mungekey` utility.
 
         Raises:
-            MungeNotFoundError: Thrown if munge is not yet installed on unit.
+            MungeManagerError: Thrown if munge is not installed on unit.
         """
         if self.__is_installed():
             logger.debug("Stopping munge daemon to generate new key.")
             self.stop()
-            if os.path.isfile("/etc/munge/munge.key"):
-                os.remove("/etc/munge/munge.key")
-            subprocess.run(["mungekey", "-c"])
+            os.remove(self.key) if os.path.isfile(self.key) else ...
+            subprocess.run(["mungekey", "-c", "-k", self.key])
             self.start()
             logger.debug("New munge key generated. Restarting munge daemon")
         else:
-            raise MungeNotFoundError(self.__class__.__name__)
+            raise MungeManagerError("Munge is not installed.")
 
-    def write_new_key(self, key: FileDataInterface) -> None:
-        """Write a new munge key to `/etc/munge/munge.key`.
+    def write_new_key(self, file: FileDataInterface) -> None:
+        """Write a new munge key.
 
         Args:
-            key (FileDataInterface): `munge.key` file received from event app.
+            file (FileDataInterface): `munge.key` file received from event app.
 
         Raises:
-            MungeNotFoundError: Thrown if munge is not yet installed on unit.
+            MungeManagerError: Thrown if munge is not installed on unit.
         """
         if self.__is_installed():
             logger.debug("Stopping munge daemon to set new munge key.")
             self.stop()
-            if os.path.isfile("/etc/munge/munge.key"):
-                os.remove("/etc/munge/munge.key")
-            key.save(key.path, update_owner=True)
-            os.chmod("/etc/munge/munge.key", stat.S_IREAD | stat.S_IWRITE)
+            file.save(file.path, update_mode=True, update_owner=True)
             logger.debug("New munge key set. Restarting munge daemon.")
             self.start()
         else:
-            raise MungeNotFoundError(self.__class__.__name__)
+            raise MungeManagerError("Munge is not installed.")
 
     def install(self) -> None:
-        """Install MUNGE."""
+        """Install munge on unit.
+
+        Raises:
+            MungeManagerError: Thrown if munge fails to install.
+        """
         try:
-            logger.debug("Installing MUNGE authentication daemon (munge).")
+            logger.debug("Installing munge authentication daemon (munge).")
             apt.add_package("munge")
         except apt.PackageNotFoundError:
             logger.error("Could not install munge. Not found in package cache.")
+            raise MungeManagerError("Failed to install munge.")
         except apt.PackageError as e:
             logger.error(f"Could not install munge. Reason: {e.message}.")
+            raise MungeManagerError("Failed to install munge.")
         finally:
-            logger.debug("munge installed.")
+            logger.debug("Munge installed.")
 
     def start(self) -> None:
-        """Start MUNGE daemon."""
+        """Start munge daemon.
+
+        Raises:
+            MungeManagerError: Thrown if munge is not installed on unit.
+        """
         if self.__is_installed():
             logger.debug("Starting munge service.")
             if not service_running("munge"):
                 service_start("munge")
-                logger.debug("munge service started.")
+                logger.debug("Munge service started.")
             else:
-                logger.debug("munge service is already running.")
+                logger.debug("Munge service is already running.")
         else:
-            raise MungeNotFoundError(self.__class__.__name__)
+            raise MungeManagerError("Munge is not installed.")
 
     def stop(self) -> None:
-        """Stop MUNGE daemon."""
+        """Stop munge daemon.
+
+        Raises:
+            MungeManagerError: Thrown if munge is not installed on unit.
+        """
         if self.__is_installed():
             logger.debug("Stopping munge service.")
             if service_running("munge"):
                 service_stop("munge")
-                logger.debug("munge service stopped.")
+                logger.debug("Munge service stopped.")
             else:
-                logger.debug("munge service is already stopped.")
+                logger.debug("Munge service is already stopped.")
         else:
-            raise MungeNotFoundError(self.__class__.__name__)
+            raise MungeManagerError("Munge is not installed.")
 
     def restart(self) -> None:
-        """Restart MUNGE daemon."""
+        """Restart munge daemon.
+
+        Raises:
+            MungeManagerError: Thrown if munge is not installed on unit.
+        """
         if self.__is_installed():
             logger.debug("Restarting munge service.")
             service_restart("munge")
-            logger.debug("munge service restarted.")
+            logger.debug("Munge service restarted.")
         else:
-            raise MungeNotFoundError(self.__class__.__name__)
+            raise MungeManagerError("Munge is not installed.")
 
     def __is_installed(self) -> bool:
         """Internal function to check in munge Debian package is installed on the unit.
@@ -181,11 +151,4 @@ class MungeManager:
         Returns:
             bool: True if Debian package is present; False if Debian package is not present.
         """
-        try:
-            munge_status = apt.DebianPackage.from_installed_package("munge")
-            if munge_status.present:
-                return True
-            else:
-                return False
-        except apt.PackageNotFoundError:
-            return False
+        return True if apt.DebianPackage.from_installed_package("munge").present else False
